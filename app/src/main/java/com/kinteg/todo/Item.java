8dc6b33b971f0ba21c71f03e604551c4c7812a51
kinteg <zeroxchao@gmail.com>
2019-09-15 11:21:36 +0300

    ToDo

--- app/src/main/java/com/kinteg/todo/Item.java
+++ app/src/main/java/com/kinteg/todo/Item.java
@@ -1,13 +1,20 @@
 package com.kinteg.todo;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 public class Item {
 
     private String name;
+    private Calendar created;
+    private Calendar closed;
+    private boolean done;
+    private int id;
 
-    public Item(String name) {
+    public Item(String name, Calendar created, int id) {
         this.name = name;
+        this.created = created;
+        this.id = id;
     }
 
     public String getName() {
@@ -18,16 +25,48 @@ public class Item {
         this.name = name;
     }
 
+    public Calendar getCreated() {
+        return created;
+    }
+
+    public void setCreated(Calendar created) {
+        this.created = created;
+    }
+
+    public Calendar getClosed() {
+        return closed;
+    }
+
+    public void setClosed(Calendar closed) {
+        this.closed = closed;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Item item = (Item) o;
-        return name.equals(item.name);
+        return id == item.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(id);
     }
 }
--- app/src/main/java/com/kinteg/todo/MainActivity.java
+++ app/src/main/java/com/kinteg/todo/MainActivity.java
@@ -2,24 +2,30 @@ package com.kinteg.todo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private final List<Item> items = new ArrayList<>();
     private final RecyclerView.Adapter adapter = new ItemAdapter(this.items);
+    private List<ConstraintLayout> layouts = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,18 +40,29 @@ public class MainActivity extends AppCompatActivity {
     public void add(View view) {
         EditText editText = this.findViewById(R.id.editText);
         if (!editText.getText().toString().equals("")) {
-            this.items.add(new Item(editText.getText().toString()));
+            this.items.add(new Item(editText.getText().toString(), Calendar.getInstance(), this.items.size()));
             editText.setText("");
             adapter.notifyItemInserted(this.items.size() - 1);
         }
     }
 
     public void delete(View view) {
-        TextView textView = (TextView) view;
-        String itemName = textView.getText().toString();
-        int itemId = this.items.indexOf(new Item(itemName.split(" ")[1]));
-        this.items.remove(itemId);
-        adapter.notifyItemRemoved(itemId);
+        ConstraintLayout constraintLayout = (ConstraintLayout) view;
+        int itemId = this.items.indexOf(new Item(null, null, Integer.parseInt(constraintLayout.getTag().toString())));
+        if (itemId != -1) {
+            this.items.remove(itemId);
+            this.layouts.remove(itemId);
+            adapter.notifyItemRemoved(itemId);
+            updateItems(itemId);
+        }
+    }
+
+    private void updateItems(int itemId) {
+        for (int i = itemId; i < items.size(); i++) {
+            this.layouts.get(i).setTag(this.items.get(i).getId());
+            TextView name = this.layouts.get(i).findViewById(R.id.name);
+            name.setText(String.format("%d. %s", i, this.items.get(i).getName()));
+        }
     }
 
     private class ItemAdapter extends RecyclerView.Adapter {
@@ -64,13 +81,41 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            ConstraintLayout constraintLayout = holder.itemView.findViewById(R.id.itemLayout);
+            layouts.add(constraintLayout);
             TextView name = holder.itemView.findViewById(R.id.name);
-            name.setText(String.format("%s. %s", position, this.items.get(position).getName()));
+            TextView created = holder.itemView.findViewById(R.id.created);
+            Item item = this.items.get(position);
+            constraintLayout.setTag(item.getId());
+            name.setText(String.format("%s. %s", position, item.getName()));
+            created.setText(format(item.getCreated()));
+            CheckBox done = holder.itemView.findViewById(R.id.done);
+            done.setOnCheckedChangeListener((view, checked) -> {
+                item.setDone(checked);
+                TextView closed = holder.itemView.findViewById(R.id.closed);
+                if (checked) {
+                    item.setClosed(Calendar.getInstance());
+                    closed.setText(format(item.getClosed()));
+                } else {
+                    closed.setText("");
+                    item.setClosed(null);
+                }
+            });
+        }
+
+        private String format(Calendar calendar) {
+            return String.format(
+                    Locale.getDefault(), "%02d.%02d.%d",
+                    calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR)
+            );
         }
 
         @Override
         public int getItemCount() {
             return this.items.size();
         }
+
+
+
     }
 }
